191dfdf4819139226fd2c0fa13df0a97f094ec92
LyanAzidie <lyan.heng18@gmail.com>
2022-05-14 01:26:33 +1000

    Add barebone class structures
    Add barebone class structures to:
    - User
    - Customer
    - Payment
    - CardPayment
    - CashPayment

---
+++ payment.py
@@ -0,0 +1,25 @@
+from database import Database
+from table import Table
+from order import Order
+from invoice import Invoice
+
+class Payment():
+    def __init__(self):
+        return
+
+    # create a new transaction
+    def create_transaction():
+        return
+
+    # check that amount paid is correct
+    def is_amount_sufficient():
+        return
+
+    # retry payment
+    def retry_payment():
+        return
+
+    # print invoice 
+    def print_invoice():
+        return 
+
